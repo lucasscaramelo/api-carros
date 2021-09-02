a00d3faab60be607e39912e4dbda16a1f161edf5
Lucas Scaramelo <lucas.scaramelo@gmail.com>
2021-09-01 22:18:14 -0300

    Regra para retornar objeto por ID

--- src/main/java/com/carros/domain/Carro.java
+++ src/main/java/com/carros/domain/Carro.java
@@ -4,11 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @AllArgsConstructor
 @Getter
 @Setter
+@Entity(name = "carro") // nome tabela
 public class Carro {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // para auto encremento no id
     private Long id;
+
+    @Column(name = "nome") // para mapear coluna
     private String nome;
 }
--- src/main/java/com/carros/domain/CarroService.java
+++ src/main/java/com/carros/domain/CarroService.java
@@ -1,14 +1,27 @@
 package com.carros.domain;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CarroService {
 
-    public List<Carro> getCarros() {
+    @Autowired
+    private CarroRepository repository;
+
+    public Iterable<Carro> getCarros() {
+        return repository.findAll();
+    }
+
+    public Optional<Carro> getCarroById(Long id) {
+        return repository.findById(id);
+    }
+
+    public List<Carro> getCarrosFake() {
         List<Carro> carros = new ArrayList<>();
         carros.add(new Carro(1L, "Civic"));
         carros.add(new Carro(2L, "Gol"));
