29dc4cff861f247e3caa56c6c164949dda2e00c3
Lucas Scaramelo <lucas.scaramelo@gmail.com>
2021-09-01 21:27:34 -0300

    Testando controller

--- src/main/java/com/carros/CarrosApplication.java
+++ src/main/java/com/carros/CarrosApplication.java
@@ -1,7 +1,10 @@
+package com.carros;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class}) // Usado o exclude apenas para teste, nao e recomendado
 public class CarrosApplication {
 
     public static void main(String[] args) {
